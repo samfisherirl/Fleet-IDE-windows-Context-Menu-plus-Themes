2869f73f57bcb8982e10db1d7de2c5cae715ab38
Sam A <98753696+samfisherirl@users.noreply.github.com>
2023-12-18 13:05:52 -0500

    Update fleetContextMenu.py

--- fleetContextMenu.py
+++ fleetContextMenu.py
@@ -1,53 +1,72 @@
-﻿
 import os
+import sys
 import winreg
+import ctypes
+import time
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def run_as_admin():
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, " ".join(sys.argv), None, 1
+    )
 
 # Define paths and menu text
 menu_text = "Open with Fleet"
-app_path = r"C:\Users\dower\AppData\Local\Programs\Fleet\Fleet.exe"
-icon_path = r"C:\Users\dower\AppData\Local\Programs\Fleet\Fleet.ico"
+appdata_local_path = os.path.join(os.getenv('LOCALAPPDATA'), 'Programs')
 
-def add_context_menu():
+# Specify the rest of the path to your executable
+executable_path = 'Fleet\Fleet.exe'
+
+# Combine the paths
+app_path = os.path.join(appdata_local_path, executable_path)
+
+def add_context_menu(location):
     """Adds a new context menu entry for folders."""
 
     # Open registry key
-    key = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, r"Directory\Background\shell", 0, winreg.KEY_WRITE)
+    key_path = f"Directory\\{location}\\Open with Fleet"
+    key = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, key_path)
+    subkey = winreg.CreateKey(key, "command")
 
-    # Create subkey for the menu entry
-    subkey = winreg.CreateKey(key, menu_text)
-
-    # **Change:** Use f-string with curly braces for command
-    winreg.SetValueEx(subkey, "command", 0, winreg.REG_SZ, f"\"{app_path}\" --dir \"%1\"")
-
-    # Set default icon for the menu entry
-    winreg.SetValueEx(subkey, "Icon", 0, winreg.REG_SZ, f"{app_path},0")
+    # Set default values
+    winreg.SetValueEx(key, 'Icon', 0, winreg.REG_SZ, f"\"{app_path}\",0")
+    winreg.SetValueEx(key, None, 0, winreg.REG_SZ, menu_text)
+    winreg.SetValueEx(subkey, None, 0, winreg.REG_SZ, f"\"{app_path}\" --dir \"%V\"")
 
     # Close registry keys
     winreg.CloseKey(subkey)
     winreg.CloseKey(key)
 
-def remove_context_menu():
+def remove_context_menu(location):
     """Removes the existing context menu entry."""
+    key_path = f"Directory\\{location}\\Open with Fleet"
 
     try:
-        # Open registry key
-        key = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, r"Directory\Background\shell", 0, winreg.KEY_DELETE)
-
-        # Delete the subkey for the menu entry
-        winreg.DeleteKey(key, menu_text)
-
-        # Close registry key
-        winreg.CloseKey(key)
+        # Delete the registry key and its subkeys
+        winreg.DeleteKey(winreg.HKEY_CLASSES_ROOT, key_path)
     except FileNotFoundError:
-        pass
+        print(f"Registry key not found: {key_path}")
+    except Exception as e:
+        print(f"Error removing context menu entry: {e}")
 
-try:
-    # Check if existing entry needs to be removed
-    if menu_text in os.listdir("HKEY_CLASSES_ROOT\\Directory\\Background\\shell"):
-        remove_context_menu()
-except:
-    print("skipping remove")
+# Check if running as admin
+if not is_admin():
+    # Re-run the script as admin
+    run_as_admin()
+    sys.exit()
+
+# Remove existing entries
+remove_context_menu("Background\\shell")
+remove_context_menu("shell")
+
 # Add new context menu entry
-add_context_menu()
+add_context_menu("Background\\shell")
+add_context_menu("shell")
 
 print(f"Successfully added '{menu_text}' context menu entry for folders.")
+time.sleep(3)
